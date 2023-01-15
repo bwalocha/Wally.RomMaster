69c8e551b5d426fd482a2b41964193f3816f413b
wally <b.walocha@gmail.com>
2023-01-15 13:15:00 +0100

    up

--- src/Wally.RomMaster.FileService.WebApi/Program.cs
+++ src/Wally.RomMaster.FileService.WebApi/Program.cs
@@ -8,34 +8,24 @@ using Microsoft.Extensions.Hosting;
 
 using Serilog;
 
-using Wally.RomMaster.FileService.Infrastructure.DI.Microsoft.Extensions;
-using Wally.RomMaster.FileService.Infrastructure.DI.Microsoft.Models;
+// using Azure.Identity;
 
 namespace Wally.RomMaster.FileService.WebApi;
 
 [ExcludeFromCodeCoverage]
 public static class Program
 {
+	private const string _azureADManagedIdentityClientIdConfigName = "AzureADManagedIdentityClientId";
+	private const string _keyVaultNameConfigName = "KeyVaultName";
 	private const bool _reloadOnChange = false;
-
-	private static IConfiguration Configuration { get; } = new ConfigurationBuilder()
-		.SetBasePath(Directory.GetCurrentDirectory())
-		.AddJsonFile("appsettings.json", false, _reloadOnChange)
-		.AddJsonFile(
-			$"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
-			true,
-			_reloadOnChange)
-		.AddJsonFile("serilog.json", true, _reloadOnChange)
-		.AddJsonFile(
-			$"serilog.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
-			true,
-			_reloadOnChange)
-		.AddEnvironmentVariables()
-		.Build();
 
 	public static int Main(string[] args)
 	{
-		Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(Configuration)
+		var configurationBuilder = new ConfigurationBuilder();
+		var configuration = ConfigureDefaultConfiguration(configurationBuilder)
+			.Build();
+
+		Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration)
 			.CreateLogger();
 
 		try
@@ -59,18 +49,45 @@ public static class Program
 		return 0;
 	}
 
+	private static IConfigurationBuilder ConfigureDefaultConfiguration(IConfigurationBuilder configurationBuilder)
+	{
+		var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+		configurationBuilder.Sources.Clear();
+
+		return configurationBuilder.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json", false, _reloadOnChange)
+			.AddJsonFile($"appsettings.{env}.json", true, _reloadOnChange)
+			.AddJsonFile("serilog.json", true, _reloadOnChange)
+			.AddJsonFile($"serilog.{env}.json", true, _reloadOnChange)
+			.AddEnvironmentVariables();
+	}
+
+	/// <summary>
+	///     https://learn.microsoft.com/en-us/azure/active-directory/managed-identities-azure-resources/tutorial-windows-vm-access-nonaad#grant-access
+	/// </summary>
+	/// <param name="configurationBuilder"></param>
+	/// <returns></returns>
+	private static IConfigurationBuilder ConfigureAppConfiguration(IConfigurationBuilder configurationBuilder)
+	{
+		/*var configuration = configurationBuilder.Build();
+		var keyVaultName = configuration[_keyVaultNameConfigName];
+		var keyVaultUrl = new Uri($"https://{keyVaultName}.vault.azure.net/");
+		var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions
+		{
+			ManagedIdentityClientId = configuration[_azureADManagedIdentityClientIdConfigName]
+		});
+
+		return configurationBuilder.AddAzureKeyVault(keyVaultUrl, credential);*/
+		return configurationBuilder;
+	}
+
 	private static IHostBuilder CreateHostBuilder(string[] args)
 	{
 		return Host.CreateDefaultBuilder(args)
+			.ConfigureAppConfiguration(a => ConfigureAppConfiguration(ConfigureDefaultConfiguration(a)))
 			.UseSerilog()
 			.UseDefaultServiceProvider(opt => { opt.ValidateScopes = true; })
-			.ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
-			.ConfigureServices(
-				services =>
-				{
-					var settings = new AppSettings();
-					Configuration.Bind(settings);
-					services.AddBackgroundServices(settings);
-				});
+			.ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
 	}
 }
