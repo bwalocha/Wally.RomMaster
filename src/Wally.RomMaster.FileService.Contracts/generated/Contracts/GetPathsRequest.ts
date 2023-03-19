95547311ebb4ce2855a4de88d41be8a793294737
wally <b.walocha@gmail.com>
2023-03-19 17:45:43 +0100

    up

--- src/Wally.RomMaster.FileService.Contracts/generated/Contracts/GetPathsRequest.ts
+++ src/Wally.RomMaster.FileService.Contracts/generated/Contracts/GetPathsRequest.ts
@@ -2,7 +2,10 @@
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
 
-export interface GetPathsRequest {
-    id: string;
-    name: string;
+import { Dayjs } from 'dayjs';
+
+export interface GetPathsRequest
+{
+	id: string;
+	name: string;
 }
