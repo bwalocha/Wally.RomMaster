f039d149b65039aa83514c2a90ea85abc6b96796
wally <b.walocha@gmail.com>
2023-03-24 22:20:49 +0100

    fix

---
+++ src/Wally.RomMaster.FileService.Application.MapperProfiles/PathProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+using Wally.RomMaster.FileService.Application.Contracts.Requests.Paths;
+using Wally.RomMaster.FileService.Application.Contracts.Responses.Paths;
+using Wally.RomMaster.FileService.Domain.Files;
+
+namespace Wally.RomMaster.FileService.Application.MapperProfiles;
+
+public class PathProfile : Profile
+{
+	public PathProfile()
+	{
+		CreateMap<Path, GetPathsRequest>();
+		CreateMap<Path, GetPathsResponse>();
+
+		// OData
+		CreateMap<GetPathsRequest, GetPathsResponse>();
+	}
+}
---
+++ src/Wally.RomMaster.FileService.WebApi/Controllers/PathsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+
+using Wally.Lib.DDD.Abstractions.Responses;
+using Wally.RomMaster.FileService.Application.Contracts.Requests.Paths;
+using Wally.RomMaster.FileService.Application.Contracts.Responses.Paths;
+using Wally.RomMaster.FileService.Application.Paths.Queries;
+
+namespace Wally.RomMaster.FileService.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+[ProducesResponseType(typeof(int), 200, "application/json")]
+public class PathsController : ControllerBase
+{
+	private readonly IMediator _mediator;
+
+	public PathsController(IMediator mediator)
+	{
+		_mediator = mediator;
+	}
+
+	/// <summary>
+	///     Gets Root Paths.
+	/// </summary>
+	/// <param name="queryOptions">OData query.</param>
+	/// <param name="cancellationToken">Cancellation token.</param>
+	/// <returns>Paths.</returns>
+	/// <remarks>
+	///     Sample request:
+	///     GET /Paths
+	/// </remarks>
+	[HttpGet]
+	public async Task<ActionResult<PagedResponse<GetPathsResponse>>> GetRootAsync(
+		ODataQueryOptions<GetPathsRequest> queryOptions,
+		CancellationToken cancellationToken)
+	{
+		var query = new GetPathsQuery(null, queryOptions);
+		var response = await _mediator.Send(query, cancellationToken);
+		return Ok(response);
+	}
+
+	/// <summary>
+	///     Gets Paths.
+	/// </summary>
+	/// <param name="parentId">Path parent Id.</param>
+	/// <param name="queryOptions">OData query.</param>
+	/// <param name="cancellationToken">Cancellation token.</param>
+	/// <returns>Paths.</returns>
+	/// <remarks>
+	///     Sample request:
+	///     GET /Paths
+	/// </remarks>
+	[HttpGet("{parentId:guid}")]
+	public async Task<ActionResult<PagedResponse<GetPathsResponse>>> GetAsync(
+		Guid? parentId,
+		ODataQueryOptions<GetPathsRequest> queryOptions,
+		CancellationToken cancellationToken)
+	{
+		var query = new GetPathsQuery(parentId, queryOptions);
+		var response = await _mediator.Send(query, cancellationToken);
+		return Ok(response);
+	}
+}
