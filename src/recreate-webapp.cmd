eb875a0d60b4060f65a3791d8e9e5d6096d54641
wally <b.walocha@gmail.com>
2024-10-05 20:30:24 +0200

    web

--- src/recreate-webapp.cmd
+++ src/recreate-webapp.cmd
@@ -1,7 +1,8 @@
 SET WEBAPP=wally.rommaster.webapp
 
+git diff --exit-code || echo "Commit your git changes" && exit /B
 rmdir .\%WEBAPP% /S /Q
-call npx create-next-app@latest --typescript --eslint %WEBAPP%
+call npx create-next-app@latest --typescript --eslint --app --use-npm %WEBAPP%
 cd ./%WEBAPP%
 call npm i
 call npm run build
