10a0b9116ec901a4da9a8e8427d1ea1434a89ed5
wally <b.walocha@gmail.com>
2024-10-05 14:26:26 +0200

    up

--- src/Wally.RomMaster.NotificationService/Wally.RomMaster.NotificationService.Application/Notifications/Commands/BroadcastNotificationCommandHandler.cs
+++ src/Wally.RomMaster.NotificationService/Wally.RomMaster.NotificationService.Application/Notifications/Commands/BroadcastNotificationCommandHandler.cs
@@ -8,18 +8,18 @@ namespace Wally.RomMaster.NotificationService.Application.Notifications.Commands
 
 public class BroadcastNotificationCommandHandler : CommandHandler<BroadcastNotificationCommand, NotificationId>
 {
-	private readonly Hub<IEventHub> _hub;
+	private readonly IHubContext<Hub<IEventHub>> _hubContext;
 
-	public BroadcastNotificationCommandHandler(Hub<IEventHub> hub)
+	public BroadcastNotificationCommandHandler(IHubContext<Hub<IEventHub>> hubContext)
 	{
-		_hub = hub;
+		_hubContext = hubContext;
 	}
 
 	public override async Task<NotificationId> HandleAsync(BroadcastNotificationCommand command, CancellationToken cancellationToken)
 	{
 		var model = Notification.Create(command.NotificationId, command.Title, command.Content);
 
-		// await _hub.Clients.All.NewEventAsync(model.Title, model.Content, cancellationToken);
+		await _hubContext.Clients.All.SendAsync("NewEventAsync", model.Title, model.Content, cancellationToken);
 		
 		return model.Id;
 	}
